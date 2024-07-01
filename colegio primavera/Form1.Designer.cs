0b3e360c449dc6892aa7002fc5a3408e01d0a536
veget <veget@ASUSE>
2024-06-30 21:29:37 -0400

    Agregar archivos de proyecto.

---
+++ colegio primavera/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace colegio_primavera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewDatos = new System.Windows.Forms.DataGridView();
+            this.ColumNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textboxApellido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewDatos
+            // 
+            this.dataGridViewDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumNombre,
+            this.ColumApellido});
+            this.dataGridViewDatos.Location = new System.Drawing.Point(253, 237);
+            this.dataGridViewDatos.Name = "dataGridViewDatos";
+            this.dataGridViewDatos.RowHeadersWidth = 51;
+            this.dataGridViewDatos.RowTemplate.Height = 24;
+            this.dataGridViewDatos.Size = new System.Drawing.Size(303, 185);
+            this.dataGridViewDatos.TabIndex = 0;
+            this.dataGridViewDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDatos_CellContentClick);
+            // 
+            // ColumNombre
+            // 
+            this.ColumNombre.HeaderText = "Nombre";
+            this.ColumNombre.MinimumWidth = 6;
+            this.ColumNombre.Name = "ColumNombre";
+            this.ColumNombre.Width = 125;
+            // 
+            // ColumApellido
+            // 
+            this.ColumApellido.HeaderText = "Apellido";
+            this.ColumApellido.MinimumWidth = 6;
+            this.ColumApellido.Name = "ColumApellido";
+            this.ColumApellido.Width = 125;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(198, 176);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(299, 176);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 2;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(408, 176);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(512, 176);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(383, 44);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNombre.TabIndex = 6;
+            // 
+            // textboxApellido
+            // 
+            this.textboxApellido.Location = new System.Drawing.Point(383, 101);
+            this.textboxApellido.Name = "textboxApellido";
+            this.textboxApellido.Size = new System.Drawing.Size(100, 22);
+            this.textboxApellido.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Apellido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxApellido);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dataGridViewDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumApellido;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textboxApellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ colegio primavera/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colegio_primavera
+{
+    public partial class Form1 : Form
+    {
+        int poc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int n = dataGridViewDatos.Rows.Add();
+
+            dataGridViewDatos.Rows[n].Cells[0].Value = textBoxNombre.Text;
+            dataGridViewDatos.Rows[n].Cells[1].Value = textboxApellido.Text;
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            dataGridViewDatos[0, poc].Value = textBoxNombre.Text;
+            dataGridViewDatos[1, poc].Value = textboxApellido.Text;
+
+        }
+
+        private void dataGridViewDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            poc = dataGridViewDatos.CurrentRow.Index;
+
+            textBoxNombre.Text = dataGridViewDatos[0, poc].Value.ToString();
+            textboxApellido.Text = dataGridViewDatos[1, poc].Value.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            dataGridViewDatos.Rows.RemoveAt(poc);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            textBoxNombre.Text = "";
+            textboxApellido.Text = "";
+            
+        }
+    }
+}
